d33812114ce1ac4eb7b61a74d5248360eb62310f
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-05-26 11:42:31 +0800

    fix mybatis config error

--- cloud-provider-payment8001/src/main/java/com/wpy/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/wpy/springcloud/controller/PaymentController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 @RestController
