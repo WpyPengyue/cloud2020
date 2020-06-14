6d7d027c9fedce5342432f56d841976bcf81f843
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-06-14 20:55:22 +0800

    load balance

---
+++ cloud-provider-payment8002/src/main/java/com/wpy/springcloud/service/PaymentService.java
@@ -0,0 +1,12 @@
+package com.wpy.springcloud.service;
+
+import com.wpy.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+
+}
