a587a51edb6aabcda22c68cdcaa8bd2784da36f0
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-07-01 21:52:03 +0800

    customer load balance

--- cloud-consumer-order80/src/main/java/com/wpy/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/wpy/springcloud/config/ApplicationContextConfig.java
@@ -9,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
-    @LoadBalanced
+    //自己写load balance时一定要把这块注释掉，不然restTemplate不能通过地址调用provider
+//    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
