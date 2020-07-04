a2dcdacaece57d6a5fb11ace54bb5baa7ea2aa23
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-07-04 16:21:26 +0800

    add hystrix support

--- cloud-provider-hystrix-payment8001/src/main/java/com/wpy/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/wpy/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.wpy.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,14 +13,22 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + " paymentInfo_OK, id: " + id + "\t";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="3000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int  time = 3;
+        int  time = 1;
+//        int age = 10 / 0;
         try {
             TimeUnit.SECONDS.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + " paymentInfo_OK, id: " + id + "\t 耗时"+ time +"秒";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池： " + Thread.currentThread().getName() + " paymentInfo_TimeOutHandler, id: " + id + "\t";
     }
 
 }
