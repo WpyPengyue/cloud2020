a6d4d6599a1393a5cd398fdf17f57841b40499a3
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-07-05 19:37:05 +0800

    add global fallback

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/wpy/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/wpy/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,6 @@
 package com.wpy.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wpy.springcloud.service.PaymentHystrixService;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
 
     @Resource
@@ -25,6 +27,7 @@ public class OrderHystrixController {
     @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
             @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="1000")
     })
+//    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     String paymentInfo_timeout(@PathVariable("id") Integer id){
         return paymentHystrixService.paymentInfo_timeout(id);
@@ -32,6 +35,11 @@ public class OrderHystrixController {
 
     public String paymentInfo_timeoutHandler(Integer id){
         return "线程池： " + Thread.currentThread().getName() + " paymentInfo_TimeOutHandler, id: " + id + "\t";
+    }
+
+    // 全局fallback方法
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试";
     }
 
 
