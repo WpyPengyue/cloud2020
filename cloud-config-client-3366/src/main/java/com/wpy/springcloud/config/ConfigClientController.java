4688157ecef5253c6b37c99863d6371265f1d3a7
Wang Pengyue <746699978@qq.com>
2020-07-17 11:37:51 +0800

    fix rabbitmq did not take effect

--- cloud-config-client-3366/src/main/java/com/wpy/springcloud/config/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/wpy/springcloud/config/ConfigClientController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
-    @Value("${server.port}")
-    private String serverPort;
-
     @Value("${config.info}")
     private String configInfo;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/configInfo")
-    private String configInfo(){
-        return "serverPort: " + serverPort + "\t\n\n configInfo: " + configInfo;
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo" + configInfo;
     }
 
 }
