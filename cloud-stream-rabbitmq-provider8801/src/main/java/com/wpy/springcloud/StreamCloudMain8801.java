cbf8cf564c3ed7c38b6a27c575c2f7f5a440d7d4
Wang Pengyue <746699978@qq.com>
2020-07-17 18:46:52 +0800

    add rabbitmq provider service

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/wpy/springcloud/StreamCloudMain8801.java
@@ -0,0 +1,15 @@
+package com.wpy.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class StreamCloudMain8801 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamCloudMain8801.class, args);
+    }
+
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/wpy/springcloud/service/IMessageProvider.java
@@ -0,0 +1,7 @@
+package com.wpy.springcloud.service;
+
+public interface IMessageProvider {
+
+    String send();
+    
+}
