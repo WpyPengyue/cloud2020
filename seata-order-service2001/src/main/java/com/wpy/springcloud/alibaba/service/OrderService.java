4d3cd4a2fdce64cf91af02cddfe16dbace6073b0
Wang Pengyue <746699978@qq.com>
2020-07-30 21:40:52 +0800

    add seata order service

---
+++ seata-order-service2001/src/main/java/com/wpy/springcloud/alibaba/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.wpy.springcloud.alibaba.service;
+
+import com.wpy.springcloud.alibaba.domain.Order;
+
+public interface OrderService {
+
+    void create(Order order);
+
+}
---
+++ seata-order-service2001/src/main/java/com/wpy/springcloud/alibaba/service/impl/OrderServiceImpl.java
@@ -0,0 +1,50 @@
+package com.wpy.springcloud.alibaba.service.impl;
+
+import com.wpy.springcloud.alibaba.dao.OrderDao;
+import com.wpy.springcloud.alibaba.domain.Order;
+import com.wpy.springcloud.alibaba.service.AccountService;
+import com.wpy.springcloud.alibaba.service.OrderService;
+import com.wpy.springcloud.alibaba.service.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    @Resource
+    private OrderDao orderDao;
+
+    @Resource
+    private StorageService storageService;
+
+    @Resource
+    private AccountService accountService;
+
+    @Override
+    public void create(Order order) {
+
+        log.info("-----> 开始新建订单");
+        // 1 新建订单
+        orderDao.create(order);
+
+        log.info("订单微服务开始调用库存，做扣减Count");
+        // 2 扣减库存
+        storageService.decrease(order.getProductId(), order.getCount());
+        log.info("订单微服务开始调用库存，做扣减End");
+        log.info("订单微服务开始调用账户，做扣减Money");
+        // 3 扣减账户
+        accountService.decrease(order.getUserId(),order.getMoney());
+        log.info("订单微服务开始调用库存，做扣减End");
+
+        // 4修改订单的状态，从0到1,1代表已完成
+        log.info("修改订单状态开始");
+        orderDao.update(order.getUserId(), 0);
+        log.info("修改订单装袋结束");
+
+        log.info("下订单结束了。。。。");
+
+    }
+}
